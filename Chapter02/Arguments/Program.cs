87e917a59604d8129298f6d6f4d1280c954d8614
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-05-20 06:14:01 +0200

    first commit

---
+++ Chapter02/Arguments/Program.cs
@@ -0,0 +1,26 @@
+﻿#region Setting Options with Args
+if (args.Length < 3)
+{
+    Console.WriteLine("You must specify two colors and cursor size, e.g.");
+    Console.WriteLine("dotnet run red yellow 50");
+    return; // Stop running.
+}
+
+/*ForegroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[0], ignoreCase: true);
+BackgroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[1], ignoreCase: true);
+CursorSize = int.Parse(args[2]);
+*/
+#endregion
+
+#region Command Line Args
+/*Console.WriteLine($"There are {args.Length} arguments.");
+
+foreach (string arg in args)
+{
+    Console.WriteLine(arg);
+}*/
+#endregion
