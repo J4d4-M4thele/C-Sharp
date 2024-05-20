5e07951c5321aa613a164d95367a332c369cdacb
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-05-20 08:00:54 +0200

    first commit

---
+++ Chapter02/Chap02Ex03Numbers/Program.cs
@@ -0,0 +1,58 @@
+﻿//SBYTE
+using System;
+
+Console.WriteLine("{0} {1} {2} {3}",
+arg0: "sbyte", arg1: "sbyte", arg2: sbyte.MinValue, arg3: sbyte.MaxValue);
+
+//BYTE
+Console.WriteLine("{0} {1} {2} {3}",
+arg0: "byte", arg1: "byte", arg2: byte.MinValue, arg3: byte.MaxValue);
+
+//SHORT
+Console.WriteLine("{0} {1} {2} {3}",
+arg0: "short", arg1: "short", arg2: short.MinValue, arg3: short.MaxValue);
+
+//USHORT
+Console.WriteLine("{0} {1} {2} {3}",
+arg0: "ushort", arg1: "ushort", arg2: ushort.MinValue, arg3: ushort.MaxValue);
+
+//INT
+Console.WriteLine("{0} {1} {2} {3}",
+arg0: "int", arg1: "int", arg2: int.MinValue, arg3: int.MaxValue);
+
+//UINT
+Console.WriteLine("{0} {1} {2} {3}",
+arg0: "uint", arg1: "uint", arg2: uint.MinValue, arg3: uint.MaxValue);
+
+//LONG
+Console.WriteLine("{0} {1} {2} {3}",
+arg0: "long", arg1: "long", arg2: long.MinValue, arg3: long.MaxValue);
+
+//ULONG
+Console.WriteLine("{0} {1} {2} {3}",
+arg0: "ulong", arg1: "ulong", arg2: ulong.MinValue, arg3: ulong.MaxValue);
+
+//INT128
+Console.WriteLine("{0} {1} {2} {3}",
+arg0: "Int128", arg1: "Int128", arg2: Int128.MinValue, arg3: Int128.MaxValue);
+
+//UINT128
+Console.WriteLine("{0} {1} {2} {3}",
+arg0: "UInt128", arg1: "UInt128", arg2: UInt128.MinValue, arg3: UInt128.MaxValue);
+
+//HALF
+Console.WriteLine("{0} {1} {2} {3}",
+arg0: "Half", arg1: "Half", arg2: Half.MinValue, arg3: Half.MaxValue);
+
+//FLOAT
+Console.WriteLine("{0} {1} {2} {3}",
+arg0: "float", arg1: "float", arg2: float.MinValue, arg3: float.MaxValue);
+
+//DOUBLE
+Console.WriteLine("{0} {1} {2} {3}",
+arg0: "double", arg1: "double", arg2: double.MinValue, arg3: double.MaxValue);
+
+//DECIMAL
+Console.WriteLine("{0} {1} {2} {3}",
+arg0: "decimal", arg1: "decimal", arg2: decimal.MinValue, arg3: decimal.MaxValue);
+
