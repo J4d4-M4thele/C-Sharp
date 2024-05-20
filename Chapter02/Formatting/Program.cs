3d425d36ae6a5e37868e455278cdab31ef02ce0b
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-05-20 06:14:39 +0200

    update

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -1,9 +1,10 @@
-﻿/*
+﻿using static System.Console;
+
 #region Numbered Positional Argments
 int numOfApples = 12;
 decimal pricePerApple = 0.35M;
 
-Console.WriteLine(
+WriteLine(
     format: "{0} apples cost {1:C}",//:C formatted as currency
     arg0: numOfApples,
     arg1: pricePerApple
@@ -15,17 +16,17 @@ string formatted = string.Format(
     arg1: pricePerApple * numOfApples
     ) ;
 
-Console.WriteLine( formatted );
+WriteLine( formatted );
 
-Console.WriteLine("{0} {1} lived in {2}.",
+WriteLine("{0} {1} lived in {2}.",
 arg0: "Roger", arg1: "Cevung", arg2: "Stockholm");
 
-Console.WriteLine( "{0} {1} lived in {2} and worked in the {3} team at {4}.",
+WriteLine( "{0} {1} lived in {2} and worked in the {3} team at {4}.",
 "Roger", "Cevung", "Stockholm", "Education", "Optimizely");
 #endregion
 
 #region Interpolated Strings
-Console.WriteLine($"{numOfApples} apples cost {pricePerApple *  numOfApples:C}.");
+WriteLine($"{numOfApples} apples cost {pricePerApple *  numOfApples:C}.");
 #endregion
 
 #region Formatted Strings
@@ -37,25 +38,33 @@ int applesCount = 1234;
 string bananasText = "Bananas";
 int bananasCount = 5678;
 
-Console.WriteLine();
+WriteLine();
 
-Console.WriteLine(format: "{0,-10} {1,6}",
+WriteLine(format: "{0,-10} {1,6}",
 arg0: "Name", arg1: "Count");
 
-Console.WriteLine(format: "{0,-10} {1,6:N0}",
+WriteLine(format: "{0,-10} {1,6:N0}",
 arg0: applesText, arg1: applesCount);
 
-Console.WriteLine(format: "{0,-10} {1,6:N0}",
+WriteLine(format: "{0,-10} {1,6:N0}",
 arg0: bananasText, arg1: bananasCount);
 #endregion
-*/
+
 #region Getting User Input
-Console.WriteLine("Type your first name and press ENTER: ");
+WriteLine("Type your first name and press ENTER: ");
 //? after data type specifies that we might have a null value
-string? firstName = Console.ReadLine();
+string? firstName = ReadLine();
 
-Console.WriteLine("Type your age and press ENTER: ");
-string age = Console.ReadLine();
+WriteLine("Type your age and press ENTER: ");
+string age = ReadLine();
 
-Console.WriteLine($"Hello {firstName}, you look good for {age}.");
+WriteLine($"Hello {firstName}, you look good for {age}.");
+#endregion
+
+#region Getting User Key Input
+Write("Press any key combination: ");
+ConsoleKeyInfo key = ReadKey();
+WriteLine();
+WriteLine("Key: {0}, Char: {1}, Modifiers: {2}",
+arg0: key.Key, arg1: key.KeyChar, arg2: key.Modifiers);
 #endregion
