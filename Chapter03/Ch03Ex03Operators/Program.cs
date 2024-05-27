840a01a12155075c0e883ef9cc383fc7218c96b3
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-05-27 16:57:52 +0200

    update

--- Chapter03/Ch03Ex03Operators/Program.cs
+++ Chapter03/Ch03Ex03Operators/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//incrementing and decrementing
+int x = 3;
+int y = 2 + ++x;
+WriteLine($"x is {x}, y is {y}");
+WriteLine();
+//binary shift operators
+x = 3 << 2;
+y = 10 >> 1;
+WriteLine($"x is {x}, y is {y}");
+WriteLine();
+//bitwise operators
+x = 10 & 8;
+y = 10 | 7;
+WriteLine($"x is {x}, y is {y}");
