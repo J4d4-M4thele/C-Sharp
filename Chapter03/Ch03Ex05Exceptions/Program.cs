a59fdf64c42c780de20374ff926f297c9f09fbdc
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-05-27 16:57:36 +0200

    first commit

---
+++ Chapter03/Ch03Ex05Exceptions/Program.cs
@@ -0,0 +1,15 @@
+﻿Write("Enter a number between 0 and 255: ");
+string? input1 = ReadLine();
+Write("Enter another number between 0 and 255: ");
+string? input2 = ReadLine();
+
+try
+{
+    int num1 = int.Parse(input1!);
+    int num2 = int.Parse(input2!);
+    WriteLine($"{num1} divided by {num2} is {num1 / num2}");
+}
+catch (FormatException e) 
+{ 
+    WriteLine($"{e.GetType}: {e.Message}.");
+}
