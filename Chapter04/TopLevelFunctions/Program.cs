fb1c323498c11224b0f1ff1681d97a4f0b0644af
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-05-28 11:10:40 +0200

    update

--- Chapter04/TopLevelFunctions/Program.cs
+++ Chapter04/TopLevelFunctions/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+#region High Level Functions
+/*
+WriteLine("* Top-level funvction examples");
+
+WhatsMyNamespace(); // Call the function.
+void WhatsMyNamespace() // Define a local function.
+{
+    WriteLine("Namespace of Program class: {0}",
+    arg0: typeof(Program).Namespace ?? "null");
+}
+*/
+#endregion
+
+#region Local Function
+/*
+partial class Program
+{
+    static void <Main>$(String[] args)
+{
+WriteLine("* Top-level functions example");
+<<Main>$>g__WhatsMyNamespace|0_0(); // Call the function.
+void <<Main>$>g__WhatsMyNamespace|0_0() // Define a local function.
+{
+WriteLine("Namespace of Program class: {0}",
+arg0: typeof(Program).Namespace ?? "null");
+}
+}
+}
+*/
+#endregion
+
+#region Partial class Reference
+/*
+WriteLine("* Top-level functions example");
+
+WhatsMyNameSpace();
+*/
+#endregion
+
+
