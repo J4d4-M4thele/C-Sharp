d3e35a506e0f98bd79c66e2575dafb49409d8cff
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-05-30 11:15:51 +0200

    first commit

---
+++ Chapter04/CallStackExceptionHandlingLib/Processor.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+namespace CallStackExceptionHandlingLib;
+
+public class Processor
+{
+    public static void Gamma()//public can be called from outside
+    {
+        WriteLine("In Gamma");
+    }
+
+    private static void Delta()// private can only be called internally
+    {
+        WriteLine("In Delta");
+        File.OpenText("bad file path");
+    }
+}
