b1db85864c44dbe77b6d6cfa7825f023130e63c6
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-04 12:07:29 +0200

    first commit

---
+++ Chapter05/PacktLibraryModern/Headset.cs
@@ -0,0 +1,11 @@
+﻿namespace Packt.Shared;
+
+public class Headset(string manufacturer, string productName)
+{
+    public string Manufacturer { get; set; } = manufacturer;
+
+    public string ProductName { get; set; } = productName;
+
+    //primary constructor
+    public Headset() : this("Microsoft", "HoloLens") { }
+}
