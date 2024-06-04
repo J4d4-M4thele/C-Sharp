fbde615476aa391ff47d934c5483e0f200728c16
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-04 11:04:44 +0200

    update

--- Chapter05/PacktLibraryNetStandard2/Person.cs
+++ Chapter05/PacktLibraryNetStandard2/Person.cs
@@ -1,9 +1,116 @@
 ﻿namespace Packt.Shared;
 
-public class Person : object
+public partial class Person : object
 {
     #region Fields: Data or state for Person 
     public string? Name;
     public DateTimeOffset Born;
+    public WondersOfTheAncientWorld FaveWonder;
+    public WondersOfTheAncientWorld BucketList;
+    public List<Person> Children = new();
+    public const string Species = "Homo Sapien";
+    public readonly string HomePlanet = "Earth";
+    public readonly DateTime Instantiated;
     #endregion
+
+    #region Constructors: Called when using new ti instantiate a type
+    public Person()
+    {
+        Name = "Unknown";
+        Instantiated = DateTime.Now;
+    }
+
+    public Person(string initialName, string homePlanet)
+    {
+        Name = initialName;
+        HomePlanet = homePlanet;
+        Instantiated = DateTime.Now;
+    }
+    #endregion
+
+    #region Methods: Actions the type can perform.
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {Born:dddd}.");
+    }
+
+    public string GetOrigin()
+    {
+        return $"{Name} was born on {HomePlanet}.";
+    }
+
+    public string SayHello()
+    {
+        return $"{Name} says 'Hello!'";
+    }
+
+    public string SayHelloTo(string name)
+    {
+        return $"{Name} says 'Hello, {name}!'";
+    }
+
+    public string OptionalParameters(int count, string command = "Run!",
+    double number = 0.0, bool active = true)
+    {
+        return string.Format(
+        format: "command is {0}, number is {1}, active is {2}",
+        arg0: command,
+        arg1: number,
+        arg2: active);
+    }
+
+    public void PassingParams(int w, in int x, ref int y, out int z) 
+    {
+        z = 100;
+        w++;
+        y++;
+        //x++ gives compile error
+        z++;
+        WriteLine($"In the method: w={w}, x={x}, y={y}, z={z}");
+    }
+
+    // Method that returns a tuple: (string, int).
+    public (string, int) GetFruit()
+    {
+        return ("Apples", 5);
+    }
+
+    // Method that returns a tuple with named fields.
+    public (string Name, int Number) GetNamedFruit()
+    {
+        return (Name: "Apples", Number: 5);
+    }
+
+    public void Deconstruct(out string? name,
+    out DateTimeOffset dob)
+    {
+        name = Name;
+        dob = Born;
+    }
+    public void Deconstruct(out string? name,
+    out DateTimeOffset dob,
+    out WondersOfTheAncientWorld fav)
+    {
+        name = Name;
+        dob = Born;
+        fav = FaveWonder;
+    }
+
+    // Method with a local function.
+    public static int Factorial(int number)
+    {
+        if (number < 0)
+        {
+            throw new ArgumentException(
+            $"{nameof(number)} cannot be less than zero.");
+        }
+        return localFactorial(number);
+        int localFactorial(int localNumber) // Local function.
+        {
+            if (localNumber == 0) return 1;
+            return localNumber * localFactorial(localNumber - 1);
+        }
+    }
+    #endregion
+
 }
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -1,7 +1,8 @@
 ﻿using Packt.Shared;
+using Fruit = (string Name, int Number); // Aliasing a tuple type.
 
 ConfigureConsole();
-
+#region Bob
 //instantiating an object
 Person bob = new();
 // WriteLine(bob);
@@ -13,9 +14,42 @@ bob.Born = new DateTimeOffset(
     hour: 16, minute: 28, second: 0,
     offset: TimeSpan.FromHours(-5)
 );
+//FAVOURITE WONDERS
+bob.FaveWonder = WondersOfTheAncientWorld.GreatPyramidOfGiza;
+bob.BucketList = WondersOfTheAncientWorld.ColossusOfRhodes | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+
 WriteLine(format: "{0} was born on {1:D}.", // Long date.
 arg0: bob.Name, arg1: bob.Born);
+WriteLine(format: "{0}'s favourite wonder is {1}. It's intger is {2}.",
+    arg0: bob.Name,
+    arg1: bob.FaveWonder,
+    arg2: (int)bob.FaveWonder
+);
+WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}.");
 
+//species is a constant field
+WriteLine($"{bob.Name} is a {Person.Species}");
+//homePlanet is a read-only field
+WriteLine($"{bob.Name} was born on {bob.HomePlanet}.");
+#endregion
+
+#region Bob's Children
+Person alfred = new Person();
+alfred.Name = "Alfred";
+bob.Children.Add(alfred);
+
+bob.Children.Add(new() { Name = "Zoe" });
+bob.Children.Add(new() { Name = "Bella" });
+
+WriteLine($"{bob.Name} has {bob.Children.Count} children:");
+for (int childIndex = 0; childIndex < bob.Children.Count; childIndex++)
+{
+    WriteLine($"> {bob.Children[childIndex].Name}");
+}
+#endregion
+
+#region Alice
+/*
 //instatiating and setting fields
 Person alice = new()
 {
@@ -26,3 +60,241 @@ TimeSpan.Zero)
 };
 WriteLine(format: "{0} was born on {1:d}.", // Short date.
 arg0: alice.Name, arg1: alice.Born);
+*/
+#endregion
+
+#region BankAccount Example
+WriteLine();
+BankAccount.InterestRate = 0.12M;
+//Jones
+BankAccount jonesAccount = new();
+jonesAccount.AccountName = "Mrs. Jones";
+jonesAccount.Balance = 2400;
+
+WriteLine(format: "{0} earned {1:C} interest.",
+arg0: jonesAccount.AccountName,
+arg1: jonesAccount.Balance * BankAccount.InterestRate);
+//Gerrier
+BankAccount gerrierAccount = new();
+gerrierAccount.AccountName = "Ms. Gerrier";
+gerrierAccount.Balance = 98;
+
+WriteLine(format: "{0} earned {1:C} interest.",
+arg0: gerrierAccount.AccountName,
+arg1: gerrierAccount.Balance * BankAccount.InterestRate);
+#endregion
+
+#region Book Example
+/*
+WriteLine();
+Book book = new()
+{ 
+    Isbn = "978-1803237800",
+    Title = "C# 12 and .NET 8 - Modern Cross-Platform Development Fundamentals"
+};
+WriteLine("{0}: {1} written by {2} has {3:N0} pages.",
+book.Isbn, book.Title, book.Author, book.PageCount);
+
+
+Book book = new(isbn: "978-1803237800", title: "C# 12 and .NET 8 - Modern Cross-Platform Development Fundamentals")
+{
+    Author = "Mark J. Price",
+    PageCount = 821
+};
+*/
+#endregion
+
+#region Using Constructors
+Person blankPerson = new();
+
+WriteLine(format: "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+    arg0: blankPerson.Name,
+    arg1: blankPerson.HomePlanet,
+    arg2: blankPerson.Instantiated);
+
+Person gunny = new(initialName: "Gunny", homePlanet: "Mars");
+WriteLine(format:
+"{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+arg0: gunny.Name,
+arg1: gunny.HomePlanet,
+arg2: gunny.Instantiated);
+#endregion
+
+#region Using Methods
+bob.WriteToConsole();
+WriteLine(bob.GetOrigin());
+
+WriteLine(bob.SayHello());
+WriteLine(bob.SayHelloTo("Emily"));
+#endregion
+
+#region Optional Parameters
+//uses default values
+/*
+WriteLine(bob.OptionalParameters());
+WriteLine(bob.OptionalParameters("Jump!", 98.5));
+WriteLine(bob.OptionalParameters(number: 52.7, command: "Hide!"));
+WriteLine(bob.OptionalParameters("Poke!", active: false));
+*/
+#endregion
+
+#region Optional and Required Parameters
+WriteLine(bob.OptionalParameters(3));
+WriteLine(bob.OptionalParameters(3, "Jump!", 98.5));
+WriteLine(bob.OptionalParameters(3, number: 52.7, command: "Hide!"));
+WriteLine(bob.OptionalParameters(3, "Poke!", active: false));
+#endregion
+
+#region Parameters
+int a = 10;
+int b = 20;
+int c = 30;
+int d = 40;
+WriteLine($"Before: a={a}, b={b}, c={c}, d={d}");
+bob.PassingParams(a, b, ref c, out d);
+WriteLine($"After: a={a}, b={b}, c={c}, d={d}");
+
+int e = 50;
+int f = 60;
+int g = 70;
+WriteLine($"Before: e={e}, f={f}, g={g}, h doesn't exist yet!");
+// Simplified C# 7 or later syntax for the out parameter.
+bob.PassingParams(e, f, ref g, out int h);
+WriteLine($"After: e={e}, f={f}, g={g}, h={h}");
+#endregion
+
+#region Tuple Fields
+(string, int) fruit = bob.GetFruit();
+WriteLine($"{fruit.Item1}, {fruit.Item2} there are.");
+
+var fruitNamed = bob.GetNamedFruit();
+WriteLine($"There are {fruitNamed.Number} {fruitNamed.Name}.");
+#endregion
+
+#region Tuple Name
+var thing1 = ("Neville", 4);
+WriteLine($"{thing1.Item1} has {thing1.Item2} children.");
+var thing2 = (bob.Name, bob.Children.Count);
+WriteLine($"{thing2.Name} has {thing2.Count} children.");
+#endregion
+
+#region Aliasing tuples
+Fruit fruitNamed2 = bob.GetNamedFruit();
+#endregion
+
+#region Deconstructing Tuples
+// Store return value in a tuple variable with two named fields.
+(string name, int number) namedFields = bob.GetNamedFruit();
+// You can then access the named fields.
+WriteLine($"{namedFields.name}, {namedFields.number}");
+
+// Deconstruct the return value into two separate variables.
+(string name, int number) = bob.GetNamedFruit();
+// You can then access the separate variables.
+WriteLine($"{name}, {number}");
+
+(string fruitName, int fruitNumber) = bob.GetFruit();
+WriteLine($"Deconstructed tuple: {fruitName}, {fruitNumber}");
+#endregion
+
+#region Deconstructor Methods
+var (name1,dob1) = bob;
+WriteLine($"Deconstructed person: {name1}, {dob1}");
+
+var (name2, dob2, fave2) = bob;
+WriteLine($"Deconstructed person: {name2}, {dob2}, {fave2}");
+#endregion
+
+#region Factorial
+int num = 5;
+try
+{
+    WriteLine($"{num}! is {Person.Factorial(num)}");
+}
+catch (Exception ex)
+{
+    WriteLine($"{ex.GetType()} says: {ex.Message} number was {num}.");
+}
+#endregion
+
+#region Person Auto Gen
+
+Person sam = new()
+{
+    Name = "Sam",
+    Born = new(1969, 6, 25, 0, 0, 0, TimeSpan.Zero)
+};
+sam.FavoriteIceCream = "Chocolate Fudge";
+WriteLine($"Sam's favorite ice-cream flavor is {sam.FavoriteIceCream}.");
+string color = "Red";
+try
+{
+    sam.FavoritePrimaryColor = color;
+    WriteLine($"Sam's favorite primary color is {sam.
+    FavoritePrimaryColor}.");
+}
+catch (Exception ex)
+{
+    WriteLine("Tried to set {0} to '{1}': {2}",
+    nameof(sam.FavoritePrimaryColor), color, ex.Message);
+}
+
+#endregion
+
+#region Defining Indexes
+sam.Children.Add(new()
+{
+    Name = "Charlie",
+    Born = new(2010, 3, 18, 0, 0, 0, TimeSpan.Zero)
+});
+sam.Children.Add(new()
+{
+    Name = "Ella",
+    Born = new(2020, 12, 24, 0, 0, 0, TimeSpan.Zero)
+});
+// Get using Children list.
+WriteLine($"Sam's first child is {sam.Children[0].Name}.");
+WriteLine($"Sam's second child is {sam.Children[1].Name}.");
+// Get using the int indexer.
+WriteLine($"Sam's first child is {sam[0].Name}.");
+WriteLine($"Sam's second child is {sam[1].Name}.");
+// Get using the string indexer.
+WriteLine($"Sam's child named Ella is {sam["Ella"].Age} years old.");
+#endregion
+
+#region Flight Patterns Example
+Passenger[] passengers = { 
+    new FirstClassPassenger {AirMiles = 1_419, Name = "Suman"},
+    new FirstClassPassenger { AirMiles = 16_562, Name = "Lucy" },
+    new BusinessClassPassenger { Name = "Janice" },
+    new CoachClassPassenger { CarryOnKG = 25.7, Name = "Dave" },
+    new CoachClassPassenger { CarryOnKG = 0, Name = "Amit" },
+};
+
+foreach (Passenger passenger in passengers)
+{
+    decimal flightCost = passenger switch
+    {
+        FirstClassPassenger p when p.AirMiles > 35_000 => 1_500M,
+        FirstClassPassenger p when p.AirMiles > 15_000 => 1_750M,
+        FirstClassPassenger _ => 2_000M,
+        BusinessClassPassenger _ => 1_000M,
+        CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
+        CoachClassPassenger _ => 650M,
+        _ => 800M
+    };
+    WriteLine();    
+}
+#endregion
+
+#region Flight Patterns Example
+
+#endregion
+
+#region Flight Patterns Example
+
+#endregion
+
+#region Flight Patterns Example
+
+#endregion
