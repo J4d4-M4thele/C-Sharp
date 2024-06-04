4c18d99826ba08e7d67882a518c8cd208fcd3a2e
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-04 11:04:58 +0200

    first commit

---
+++ Chapter05/PacktLibraryModern/Book.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Packt.Shared;
+
+public class Book
+{
+    public required string? Isbn;
+    public required string? Title;
+
+    public string? Author;
+    public int PageCount;
+
+    //constructors
+    public Book() { }
+
+    [SetsRequiredMembers]
+    public Book(string isbn, string title)
+    {
+        Isbn = isbn;
+        Title = title;
+    }
+}
