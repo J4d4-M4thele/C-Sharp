592d9ea6b8e030716f2354e828e2394adb9a16a8
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-05 13:32:22 +0200

    first commit

---
+++ Chapter06/PeopleApp/Program.EventHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Packt.Shared;
+
+partial class Program
+{
+    private static void Harry_Shout(object? sender, EventArgs e)
+    { 
+        //do nothing if no sender
+        if(sender is null) return;
+
+        //do nothing if sender is not a person
+        if(sender is not Person p) return;
+
+        WriteLine($"{p.Name} has level {p.AngerLevel} anger.");
+    }
+}
