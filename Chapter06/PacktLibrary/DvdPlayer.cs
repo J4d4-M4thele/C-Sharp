112444538399572c3dadef20097262ffce0ccb02
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-05 14:50:11 +0200

    first commit

---
+++ Chapter06/PacktLibrary/DvdPlayer.cs
@@ -0,0 +1,14 @@
+﻿namespace Packt.Shared;
+
+internal class DvdPlayer : IPlayable
+{
+    public void Pause()
+    {
+        WriteLine("DVD player has paused the music.");
+    }
+
+    public void Play()
+    {
+        WriteLine("DVD player is playing music.");
+    }
+}
