8d7b68f75a6ec9ad428b6a61b3718d4a549fb21f
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-06 10:39:24 +0200

    update

--- Chapter06/NullHandling/Program.cs
+++ Chapter06/NullHandling/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region Making Type Nullable
+int? couldNotBeNull = 4;
+//couldBeNull = null; //compile error
+WriteLine(couldNotBeNull);
+
+int? couldBeNull = null;
+WriteLine(couldBeNull);
+WriteLine(couldBeNull.GetValueOrDefault());
+
+couldBeNull = 7;
+WriteLine(couldBeNull);
+WriteLine(couldBeNull.GetValueOrDefault());
+#endregion
+
+#region
+
+#endregion
+
+#region
+
+#endregion
+
+#region
+
+#endregion
+
+#region
+
+#endregion
+
+#region
+
+#endregion
+
+#region
+
+#endregion
