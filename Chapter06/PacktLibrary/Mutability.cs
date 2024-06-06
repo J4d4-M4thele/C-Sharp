3598805143bb0f2cbaee1546c79a13308eb2aace
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-06 13:45:42 +0200

    first commit

---
+++ Chapter06/PacktLibrary/Mutability.cs
@@ -0,0 +1,23 @@
+﻿namespace Packt.Shared;
+
+//mutable
+public record class C1
+{
+    public string? Name { get; set; }
+}
+
+//immutable
+public record class C2(string? Name);
+
+//mutable
+public record struct S1
+{
+    public string? Name { get; set; }
+}
+
+//mutable
+public record struct S2(string? Name);
+
+//immutable
+public readonly record struct S3(string? Name);
+
