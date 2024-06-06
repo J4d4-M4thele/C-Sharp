ee23439d0364d9b7297b475e0120db4ae034cdc4
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-06 09:56:10 +0200

    update

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -173,7 +173,16 @@ WriteLine($"p1.Name == p2.Name: {p1.Name == p2.Name}");
 #endregion
 
 #region Struct Types
+DisplacementVector dv1 = new(3, 5);
+DisplacementVector dv2 = new(-2, 7);
+DisplacementVector dv3 = dv1 + dv2;
+DisplacementVector dv4 = new();
+DisplacementVector dv5 = new(3, 5);
 
+WriteLine($"({dv1.X}, {dv1.Y}) + ({dv2.X}, {dv2.Y}) = ({dv3.X},{ dv3.Y})");
+WriteLine($"({dv4.X}, {dv4.Y})");
+WriteLine($"dv1.Equals(dv5): {dv1.Equals(dv5)})");
+//WriteLine($"dv1 == dv5: {dv1 == dv5})"); //gives error unless record struct
 #endregion
 
 #region 
