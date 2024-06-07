741d47c7877ac8f67d363f1e947e73ebf21cb41d
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-07 10:43:47 +0200

    update

--- Chapter07/AssembliesAndNamespaces/Program.cs
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml.Linq;
+using System;
+
+#region Importing Namespaces to use Types
+XDocument doc = new();
+#endregion
+
+#region C# Keywords and .NET types
+string s1 = "Hello";
+string s2= "World";
+WriteLine($"{s1} {s2}");
+#endregion
+
+#region Native Integers
+//native ints are platform specific
+//32-bit process sizeof() returns 4 bytes
+//64-bit process sizeof() returns 8 bytes
+WriteLine($"Environment.Is64BitProcess = {Environment.Is64BitProcess}");
+WriteLine($"int.MaxValue = {int.MaxValue:N0}");
+WriteLine($"nint.MaxValue = {nint.MaxValue:N0}");
+#endregion
+
+#region
+
+#endregion
+
+#region
+
+#endregion
+
+#region
+
+#endregion
