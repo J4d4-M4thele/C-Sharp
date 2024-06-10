be64d6bef38000572f51fc92bf91251a19203aa8
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-10 13:43:16 +0200

    first commit

---
+++ Chapter08/WorkingWithRegularExpressions/Program.Regexs.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions; //for [GeneratedRegex]
+
+partial class Program
+{
+    [GeneratedRegex(DigitsOnlyText, RegexOptions.IgnoreCase)]
+    private static partial Regex DigitsOnly();
+
+    [GeneratedRegex(DigitsOnlyText, RegexOptions.IgnoreCase)]
+    private static partial Regex CommaSeparator();
+}
