dee1f0edc463146c0105a8ee0eb99312bf61a747
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-10 16:00:46 +0200

    first commit

--- Chapter08/WorkingWithCollections/Program.cs
+++ Chapter08/WorkingWithCollections/Program.cs
@@ -1,5 +1,7 @@
 ﻿using StringDictionary = System.Collections.Generic.Dictionary<string,
 string>;
+using System.Collections.Immutable; //for ImmutableDictionary<T, T>
+using System.Collections.Frozen; //for FrozenDictionary<T, T>
 
 #region Lists
 /*
@@ -22,7 +24,7 @@ OutputCollection("After removing two cities", cities);
 #endregion
 
 #region Dictionaries
-/*
+
 //dictionaries are hashmaps
 StringDictionary keywords = new();
 //named parameters
@@ -43,7 +45,7 @@ foreach (KeyValuePair<string,string> item in keywords)
 //looking up information
 string key = "long";
 WriteLine($"The definition of {key} is {keywords[key]}");
-*/
+
 #endregion
 
 #region Sets
@@ -61,6 +63,7 @@ WriteLine($"names set: {string.Join(",", names)}");
 #endregion
 
 #region Queues
+/*
 Queue<string> coffee = new();
 coffee.Enqueue("Damir"); // Front of the queue.
 coffee.Enqueue("Andrea");
@@ -81,12 +84,58 @@ OutputCollection("Current queue from front to back", coffee);
 
 WriteLine($"{coffee.Peek()} is next in line.");
 OutputCollection("Current queue from front to back", coffee);
+
+PriorityQueue<string, int> vaccine = new();
+// Add some people.
+// 1 = High priority people in their 70s or poor health.
+// 2 = Medium priority e.g. middle-aged.
+// 3 = Low priority e.g. teens and twenties.
+vaccine.Enqueue("Pamela", 1);
+vaccine.Enqueue("Rebecca", 3);
+vaccine.Enqueue("Juliet", 2);
+vaccine.Enqueue("Ian", 1);
+
+OutputPQ("Current queue for vaccination", vaccine.UnorderedItems);
+
+WriteLine($"{vaccine.Dequeue()} has been vaccinated.");
+WriteLine($"{vaccine.Dequeue()} has been vaccinated.");
+OutputPQ("Current queue for vaccination", vaccine.UnorderedItems);
+
+WriteLine($"{vaccine.Dequeue()} has been vaccinated.");
+WriteLine("Adding Mark to queue with priority 2.");
+
+vaccine.Enqueue("Mark", 2);
+
+WriteLine($"{vaccine.Peek()} will be next to be vaccinated.");
+OutputPQ("Current queue for vaccination", vaccine.UnorderedItems);
+*/
 #endregion
 
-#region
+#region Sorting Collections
+/*
+//UseDictonary(keywords);
+//UseDictonary(keywords.AsReadOnly());
+UseDictonary(keywords.ToImmutableDictionary());
 
+ImmutableDictionary<string, string> immutableKeywords = 
+    keywords.ToImmutableDictionary();
+
+ImmutableDictionary<string, string> newDictionary =
+    immutableKeywords.Add(
+        key: Guid.NewGuid().ToString(),
+        value: Guid.NewGuid().ToString());
+
+OutputCollection("Immutable keywords dictionary", immutableKeywords);
+OutputCollection("New keywords dictionary", newDictionary);
+*/
 #endregion
 
-#region
+#region Frozen Collections
+FrozenDictionary<string, string> frozenKeywords =
+keywords.ToFrozenDictionary();
 
+OutputCollection("Frozen keywords dictionary", frozenKeywords);
+
+// Lookups are faster in a frozen dictionary.
+WriteLine($"Define long: {frozenKeywords["long"]}");
 #endregion
---
+++ Chapter08/WorkingWithRanges/Program.cs
@@ -0,0 +1,15 @@
+﻿string name = "Samantha Jones";
+
+int firstLen = name.IndexOf(' ');
+int lastLen = name.Length - firstLen - 1;
+
+string firstName = name.Substring(0, firstLen);
+string lastName = name.Substring(name.Length - lastLen, lastLen);
+
+WriteLine($"First: {firstName}, Last: {lastName}");
+
+// Using spans.
+ReadOnlySpan<char> nameAsSpan = name.AsSpan();
+ReadOnlySpan<char> firstNameSpan = nameAsSpan[0..firstLen];
+ReadOnlySpan<char> lastNameSpan = nameAsSpan[^lastLen..];
+WriteLine($"First: {firstNameSpan}, Last: {lastNameSpan}");
