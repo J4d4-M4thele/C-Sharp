7259a39eafe62287c26bb0352bebc29d6144886e
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-11 14:03:44 +0200

    first commit

---
+++ Chapter09/WorkingWithStreams/Program.Helpers.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+partial class Program
+{
+    private static void SectionTitle(string title)
+    {
+        WriteLine();
+        ConsoleColor prevColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.DarkYellow;
+        WriteLine($"*** {title} ***");
+        ForegroundColor = prevColor;
+    }
+
+    private static void OutputFileInfo(string path)
+    {
+        WriteLine("**** File Info ****");
+        WriteLine($"File: {GetFileName(path)}");
+        WriteLine($"Path: {GetDirectoryName(path)}");
+        WriteLine($"Size: {new FileInfo(path).Length:N0} bytes.");
+        WriteLine("/------------------");
+        WriteLine(File.ReadAllText(path));
+        WriteLine("------------------/");
+    }
+}
