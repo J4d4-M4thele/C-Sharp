2e69d8fb66c689445423fd5ee885e900d889af2b
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-11 14:07:20 +0200

    first commit

---
+++ Chapter09/WorkingWithStreams/Viper.cs
@@ -0,0 +1,10 @@
+﻿namespace Packt.Shared;
+
+public static class Viper
+{
+    public static string[] Callsigns = new[]
+    {
+        "Husker", "Starbuck", "Apollo",
+        "Bulldog", "Athena", "Helo", "Racetrack"
+    };
+}
