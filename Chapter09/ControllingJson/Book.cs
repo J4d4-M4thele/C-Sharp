7375a7b203c4491eaec4d6bb048f3c8f73566be3
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-12 12:59:38 +0200

    update

--- Chapter09/ControllingJson/Book.cs
+++ Chapter09/ControllingJson/Book.cs
@@ -4,4 +4,19 @@ namespace Packt.Shared;
 
 public class Book
 {
+    public Book(string title)
+    {  Title = title; }
+
+    //properties
+    public string Title { get; set; }
+    public string? Author { get; set; }
+
+    //fields
+    [JsonInclude]
+    public DateTime PublishDate;
+
+    [JsonInclude]
+    public DateTimeOffset Created;
+
+    public ushort Pages;
 }
