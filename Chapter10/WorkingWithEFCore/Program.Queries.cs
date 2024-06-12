8ae56015eaa77e8c7ad69ef70ba2ea135ba5f45d
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-12 18:18:23 +0200

    first commit

---
+++ Chapter10/WorkingWithEFCore/Program.Queries.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore; //for Include method
+using Northwind.EntityModels; //for Northwind, Category, Product
+
+partial class Program
+{
+    private static void QueryCategories()
+    {
+        using NorthwindDb db = new();
+
+        SectionTitle("Categories and how many products they have");
+
+        IQueryable<Category>? categories = db.Categories?
+            .Include(c => c.Products);
+
+        if(categories is null || !categories.Any())
+        {
+            Fail("No categories found.");
+            return;
+        }
+
+        foreach (Category c in categories)
+        {
+            WriteLine($"{c.CategoryName} has {c.Products.Count} products.");
+        }
+    }
+}
