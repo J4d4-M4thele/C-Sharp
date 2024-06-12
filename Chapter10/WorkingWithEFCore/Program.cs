ca69c5e8507604511cd1241285b031dc5c115228
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-12 18:18:07 +0200

    update

--- Chapter10/WorkingWithEFCore/Program.cs
+++ Chapter10/WorkingWithEFCore/Program.cs
@@ -1,3 +1,9 @@
 ﻿using Northwind.EntityModels;
 using NorthwindDb db = new();
+
+//listing the provider
 WriteLine($"Provider: {db.Database.ProviderName}");
+
+//calling methods from partial classes
+ConfigureConsole();
+QueryCategories();
