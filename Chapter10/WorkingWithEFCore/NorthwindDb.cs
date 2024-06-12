ca940bfa6e8a5ca7a0825ec430e5db9be8312d97
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-12 17:51:37 +0200

    update

--- Chapter10/WorkingWithEFCore/NorthwindDb.cs
+++ Chapter10/WorkingWithEFCore/NorthwindDb.cs
@@ -4,6 +4,10 @@ namespace Northwind.EntityModels;
 
 public class NorthwindDb : DbContext
 {
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
+
+
     //method from DbContext superclass
     protected override void OnConfiguring(
         DbContextOptionsBuilder optionsBuilder)
@@ -15,5 +19,21 @@ public class NorthwindDb : DbContext
         string connectionString = $"Data Source={path}";
         WriteLine($"Connection: {connectionString}");
         optionsBuilder.UseSqlite(connectionString);
+    }
+
+    protected override void OnModelCreating(
+        ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>()
+            .Property(category => category.CategoryName)
+            .IsRequired()
+            .HasMaxLength(15);
+
+        if (Database.ProviderName?.Contains("sqlite") ?? false)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(product => product.Cost)
+                .HasConversion<double>();
+        }
     }
 }
