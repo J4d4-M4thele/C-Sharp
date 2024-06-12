1c528f1824a45fdbe44221e78be5e88ade40ee58
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-12 14:51:18 +0200

    update

--- Chapter09/WorkingWithEnvVars/Program.cs
+++ Chapter09/WorkingWithEnvVars/Program.cs
@@ -1,9 +1,30 @@
-﻿#region
+﻿#region Reading Environmental Variables
+SectionTitle("Reading all environment variables for process");
+IDictionary vars = GetEnvironmentVariables();
+DictionaryToTable(vars);
 
+SectionTitle("Reading all environment variables for machine");
+IDictionary varsMachine = GetEnvironmentVariables(
+    EnvironmentVariableTarget.Machine);
+DictionaryToTable(varsMachine);
+
+SectionTitle("Reading all environment variables for user");
+IDictionary varsUser = GetEnvironmentVariables(
+    EnvironmentVariableTarget.User);
+DictionaryToTable(varsUser);
 #endregion
 
-#region
+#region Expanding, setting, and getting environmental variables
+string myComputer = "My username is %USERNAME%. My CPU is %PROCESSOR_IDENTIFIER%.";
 
+WriteLine(ExpandEnvironmentVariables(myComputer));
+
+string password_key = "MY_PASSWORD";
+
+SetEnvironmentVariable(password_key, "Pa$$w0rd");
+
+string? password = GetEnvironmentVariable(password_key);
+WriteLine($"{password_key}: {password}");
 #endregion
 
 #region
