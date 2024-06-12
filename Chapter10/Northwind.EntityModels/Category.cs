4485699427a98b656614a0b98dc92ad50d376bcc
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-12 17:51:50 +0200

    first commit

---
+++ Chapter10/Northwind.EntityModels/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema; //for [Column]
+
+namespace Northwind.EntityModels;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    [Column(TypeName = "ntext")]
+    public string? Description { get; set; }
+
+    //defines a navigation property for related rows
+    public virtual ICollection<Product> Products { get; set; } = new HashSet<Product>();
+}
---
+++ Chapter10/Northwind.EntityModels/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations; //for [Required]
+using System.ComponentModel.DataAnnotations.Schema; //for [Column]
+
+namespace Northwind.EntityModels;
+
+public class Product
+{
+    public int ProductId { get; set; }
+
+    [Required]
+    [StringLength(40)]
+    public string ProductName { get; set; } = null!;
+
+    [Column("UnitPrice", TypeName = "money")]
+    public decimal? Cost { get; set; }
+
+    [Column("UnitsInStock", TypeName = "money")]
+    public short? Stock { get; set;}
+
+    public bool Discontinued { get; set; }
+
+    //foreign key relationship
+    public int CategoryId { get; set; }
+
+    public virtual Category Category { get; set; } = null!;
+}
