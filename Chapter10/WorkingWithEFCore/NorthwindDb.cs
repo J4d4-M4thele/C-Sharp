6848ee1d129ac2cf11e8025c72115a73a22c61b1
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-12 17:03:46 +0200

    first commit

---
+++ Chapter10/WorkingWithEFCore/NorthwindDb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore; // for DbContext
+
+namespace Northwind.EntityModels;
+
+public class NorthwindDb : DbContext
+{
+    //method from DbContext superclass
+    protected override void OnConfiguring(
+        DbContextOptionsBuilder optionsBuilder)
+    {
+        string databaseFile = "Northwind.db";
+        string path = Path.Combine(
+            Environment.CurrentDirectory, databaseFile);
+
+        string connectionString = $"Data Source={path}";
+        WriteLine($"Connection: {connectionString}");
+        optionsBuilder.UseSqlite(connectionString);
+    }
+}
