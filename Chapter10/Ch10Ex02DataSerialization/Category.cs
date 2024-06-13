f306185d4b093c79debd712dc69d31fd3534f49b
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-13 14:33:21 +0200

    first commit

---
+++ Chapter10/Ch10Ex02DataSerialization/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema; //for [Column]
+
+namespace Packt.Shared;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    [Column(TypeName = "ntext")]
+    public string? Description { get; set; }
+
+    //defines a navigation property for related rows
+    public virtual ICollection<Product> Products { get; set; } = new HashSet<Product>();
+}
---
+++ Chapter10/Ch10Ex02DataSerialization/Program.Functions.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml; // XmlWriter
+using System.Text.Json; // Utf8JsonWriter, JsonWriterOptions
+using Packt.Shared; // Category, Product
+
+using static System.IO.Path;
+using static System.Environment;
+
+partial class Program
+{
+
+}
