9ba9070e0e071f9c16d4051b2d0003c991a7c3db
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-13 16:06:06 +0200

    first commit

---
+++ Chapter11/LinqWithObjects/Program.Functions.cs
@@ -0,0 +1,13 @@
+﻿partial class Program
+{
+    private static void DeferredExecution(string[] names)
+    {
+        SectionTitle("Deferred execution");
+
+        //LINQ extension method
+        var query1 = names.Where(name => name.EndsWith("m"));
+
+        //LINQ query comprehension syntax
+        var query2 = from name in names where name.EndsWith("m") select name; 
+    }
+}
