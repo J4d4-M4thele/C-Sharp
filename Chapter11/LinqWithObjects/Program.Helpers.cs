a57e47b260c29f079e52e631a5bceaec57ab85d7
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-13 15:55:41 +0200

    first commit

---
+++ Chapter11/LinqWithObjects/Program.Helpers.cs
@@ -0,0 +1,10 @@
+﻿partial class Program
+{
+    private static void SectionTitle(string title)
+    {
+        ConsoleColor prevColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.DarkYellow;
+        WriteLine($"*** {title} ***");
+        ForegroundColor = prevColor;
+    }
+}
