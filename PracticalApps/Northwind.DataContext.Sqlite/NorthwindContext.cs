48d8e5d037d581a6921f777edc37e87a62581921
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-02-05 14:43:59 +0200

    update

--- PracticalApps/Northwind.DataContext.Sqlite/NorthwindContext.cs
+++ PracticalApps/Northwind.DataContext.Sqlite/NorthwindContext.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-using Northwind.DataContext.Sqlite;
-
-namespace Northwind.EntityModels;
+using Northwind.EntityModels;
 
 public partial class NorthwindContext : DbContext
 {
@@ -16,27 +14,27 @@ public partial class NorthwindContext : DbContext
     {
     }
 
-    /*
-      public virtual DbSet<Category> Categories { get; set; }
 
-      public virtual DbSet<Customer> Customers { get; set; }
+    public virtual DbSet<Category> Categories { get; set; }
 
-      public virtual DbSet<Employee> Employees { get; set; }
+    public virtual DbSet<Customer> Customers { get; set; }
 
-      public virtual DbSet<EmployeeTerritory> EmployeeTerritories { get; set; }
+    public virtual DbSet<Employee> Employees { get; set; }
 
-      public virtual DbSet<Order> Orders { get; set; }
+    public virtual DbSet<EmployeeTerritory> EmployeeTerritories { get; set; }
 
-      public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+    public virtual DbSet<Order> Orders { get; set; }
 
-      public virtual DbSet<Product> Products { get; set; }
+    public virtual DbSet<OrderDetail> OrderDetails { get; set; }
 
-      public virtual DbSet<Shipper> Shippers { get; set; }
+    public virtual DbSet<Product> Products { get; set; }
 
-      public virtual DbSet<Supplier> Suppliers { get; set; }
+    public virtual DbSet<Shipper> Shippers { get; set; }
 
-      public virtual DbSet<Territory> Territories { get; set; }
-    */
+    public virtual DbSet<Supplier> Suppliers { get; set; }
+
+    public virtual DbSet<Territory> Territories { get; set; }
+
 
     protected override void OnConfiguring(DbContextOptionsBuilder
   optionsBuilder)
@@ -69,41 +67,41 @@ public partial class NorthwindContext : DbContext
         }
     }
 
-    /*
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
-  {
-    modelBuilder.Entity<Order>(entity =>
     {
-      entity.Property(e => e.Freight).HasDefaultValueSql("0");
-    });
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.Property(e => e.Freight).HasDefaultValueSql("0");
+        });
 
-    modelBuilder.Entity<OrderDetail>(entity =>
-    {
-      entity.Property(e => e.Quantity).HasDefaultValueSql("1");
-      entity.Property(e => e.UnitPrice).HasDefaultValueSql("0");
+        modelBuilder.Entity<OrderDetail>(entity =>
+        {
+            entity.Property(e => e.Quantity).HasDefaultValueSql("1");
+            entity.Property(e => e.UnitPrice).HasDefaultValueSql("0");
 
-      entity.HasOne(d => d.Order).WithMany(p => 
-        p.OrderDetails).OnDelete(DeleteBehavior.ClientSetNull);
+            entity.HasOne(d => d.Order).WithMany(p =>
+          p.OrderDetails).OnDelete(DeleteBehavior.ClientSetNull);
 
-      entity.HasOne(d => d.Product).WithMany(p => 
-        p.OrderDetails).OnDelete(DeleteBehavior.ClientSetNull);
-    });
+            entity.HasOne(d => d.Product).WithMany(p =>
+          p.OrderDetails).OnDelete(DeleteBehavior.ClientSetNull);
+        });
 
-    modelBuilder.Entity<Product>(entity =>
-    {
-      entity.Property(e => e.Discontinued).HasDefaultValueSql("0");
-      entity.Property(e => e.ReorderLevel).HasDefaultValueSql("0");
-      entity.Property(e => e.UnitPrice).HasDefaultValueSql("0");
-      entity.Property(e => e.UnitsInStock).HasDefaultValueSql("0");
-      entity.Property(e => e.UnitsOnOrder).HasDefaultValueSql("0");
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Discontinued).HasDefaultValueSql("0");
+            entity.Property(e => e.ReorderLevel).HasDefaultValueSql("0");
+            entity.Property(e => e.UnitPrice).HasDefaultValueSql("0");
+            entity.Property(e => e.UnitsInStock).HasDefaultValueSql("0");
+            entity.Property(e => e.UnitsOnOrder).HasDefaultValueSql("0");
 
-      entity.Property(product => product.UnitPrice)
-        .HasConversion<double>();
-    });
+            entity.Property(product => product.UnitPrice)
+          .HasConversion<double>();
+        });
 
-    OnModelCreatingPartial(modelBuilder);
-  }
+        OnModelCreatingPartial(modelBuilder);
+    }
 
-  partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
-    */
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
 }
--- PracticalApps/Northwind.DataContext.Sqlite/NorthwindContextLogger.cs
+++ PracticalApps/Northwind.DataContext.Sqlite/NorthwindContextLogger.cs
@@ -1,16 +1,16 @@
 ﻿using static System.Environment;
 
-namespace Northwind.DataContext.Sqlite;
+namespace Northwind.EntityModels;
 
 public class NorthwindContextLogger
 {
-    public static void WriteLine(string message)
-    {
-        string path = Path.Combine(GetFolderPath(
-            SpecialFolder.DesktopDirectory), "northwindlog.txt");
+  public static void WriteLine(string message)
+  {
+    string path = Path.Combine(GetFolderPath(
+      SpecialFolder.DesktopDirectory), "northwindlog.txt");
 
-        StreamWriter textFile = File.AppendText(path);
-        textFile.WriteLine(message);
-        textFile.Close();
-    }
+    StreamWriter textFile = File.AppendText(path);
+    textFile.WriteLine(message);
+    textFile.Close();
+  }
 }
