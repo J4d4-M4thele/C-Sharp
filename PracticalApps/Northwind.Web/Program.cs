19f673b640ae2ad8f2b970001089bf3708f0025d
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-02-06 09:17:26 +0200

    update

--- PracticalApps/Northwind.Web/Program.cs
+++ PracticalApps/Northwind.Web/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 #region Configuring Http pipeline and routes
+//if app is not in development mode
 if(!app.Environment.IsDevelopment())
 {
     app.UseHsts();
@@ -9,9 +10,12 @@ if(!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles();//will serve .html files
+app.UseStaticFiles();
+
+app.MapGet("/hello", () => $"Environment is {app.Environment.EnvironmentName}");
 #endregion
 //starts web server, hosts website an waits for requests
 app.Run();//thread-blocking call
 
-WriteLine("This executes after web server has stopped");
+WriteLine("This executes after web server has stopped!");
