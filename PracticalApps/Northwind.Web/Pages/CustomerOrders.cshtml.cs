d228b8977a94e183d04d9f0d45c1cca764a9b6b4
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-02-10 11:10:29 +0200

    update

--- PracticalApps/Northwind.Web/Pages/CustomerOrders.cshtml.cs
+++ PracticalApps/Northwind.Web/Pages/CustomerOrders.cshtml.cs
@@ -1,12 +1,25 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.RazorPages; // To use PageModel.
+using Microsoft.EntityFrameworkCore; // To use Include method.
+using Northwind.EntityModels; // To use Customer.
 
-namespace Northwind.Web.Pages
+namespace Northwind.Web.Pages;
+
+public class CustomerOrdersModel : PageModel
 {
-    public class CustomerOrdersModel : PageModel
+    public Customer? Customer;
+
+    private NorthwindContext _db;
+
+    public CustomerOrdersModel(NorthwindContext db)
     {
-        public void OnGet()
-        {
-        }
+        _db = db;
+    }
+
+    public void OnGet()
+    {
+        string? id = HttpContext.Request.Query["id"];
+
+        Customer = _db.Customers.Include(c => c.Orders)
+          .SingleOrDefault(c => c.CustomerId == id);
     }
 }
