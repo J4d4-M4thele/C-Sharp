d45197f10407f5206c14db53f441205707c70ebd
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-02-10 10:39:24 +0200

    update

--- PracticalApps/Northwind.Web/Program.cs
+++ PracticalApps/Northwind.Web/Program.cs
@@ -19,13 +19,33 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.Use(async (HttpContext context, Func<Task> next) =>
+{
+    RouteEndpoint? rep = context.GetEndpoint() as RouteEndpoint;
+    if (rep is not null)
+    {
+        WriteLine($"Endpoint name: {rep.DisplayName}");
+        WriteLine($"Endpoint route pattern: {rep.RoutePattern.RawText}");
+    }
+    if (context.Request.Path == "/bonjour")
+    {
+        // In the case of a match on URL path, this becomes a terminating
+        // delegate that returns so does not call the next delegate.
+        await context.Response.WriteAsync("Bonjour Monde!");
+        return;
+    }
+    // We could modify the request before calling the next delegate.
+    await next();
+    // We could modify the response after calling the next delegate.
+});
+
 app.UseHttpsRedirection();
 
 app.UseDefaultFiles(); // index.html, default.html, and so on.
 app.UseStaticFiles();
 
 app.MapRazorPages();
-app.MapGet("/hello", () =>
+app.MapGet("/", () =>
   $"Environment is {app.Environment.EnvironmentName}");
 
 #endregion
