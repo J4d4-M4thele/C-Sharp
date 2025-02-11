addbc6dfc0c242208a2d49d2eded585b2d4c3977
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-02-11 11:20:13 +0200

    first commit

---
+++ PracticalApps/Northwind.WebApi/Controllers/CustomersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind.EntityModels;
+using Northwind.WebApi.Repositories;
+
+namespace Northwind.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CustomersController : Controller
+{
+    private readonly ICustomerRepository _repo;
+
+    //initialise repository
+    public CustomersController(ICustomerRepository repo)
+    {
+        _repo = repo;
+    }
+}
