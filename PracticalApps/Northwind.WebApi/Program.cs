22d2e47290e7d4d9672790221a32e452ecf7f6dc
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-02-11 10:51:23 +0200

    update

--- PracticalApps/Northwind.WebApi/Program.cs
+++ PracticalApps/Northwind.WebApi/Program.cs
@@ -1,8 +1,39 @@
+//CH 14
+using Microsoft.AspNetCore.Mvc.Formatters; // To use IOutputFormatter.
+using Northwind.EntityModels; // To use AddNorthwindContext method.
+using Microsoft.Extensions.Caching.Memory;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//in-memory cache(CH 14)
+builder.Services.AddSingleton<IMemoryCache>(new MemoryCache(new MemoryCacheOptions()));
 
-builder.Services.AddControllers();
+//add Northwind context (CH 14)
+builder.Services.AddNorthwindContext();
+
+// Add services to the container.
+builder.Services.AddControllers(options =>
+{
+    WriteLine("Default output formatters: ");
+    foreach (IOutputFormatter formatter in options.OutputFormatters)
+    {
+        OutputFormatter? mediaFormatter = formatter as OutputFormatter;
+        if (mediaFormatter is null)
+        {
+            WriteLine($"    {formatter.GetType().Name}");
+        }
+        else
+        {
+            WriteLine("{0}, Media types: {1}",
+                arg0: mediaFormatter.GetType().Name,
+                arg1: string.Join(", ",
+                mediaFormatter.SupportedMediaTypes));
+        }
+    }
+})
+    .AddXmlDataContractSerializerFormatters()
+    .AddXmlSerializerFormatters();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
