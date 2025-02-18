5cb8a9da4537f6f60ce23ffe2dd53f5c82d31ac6
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-02-18 11:48:23 +0200

    update

--- PracticalApps/Northwind.Blazor/Program.cs
+++ PracticalApps/Northwind.Blazor/Program.cs
@@ -6,7 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
   .AddInteractiveServerComponents();
+
 builder.Services.AddNorthwindContext();
+
 builder.Services.AddTransient<INorthwindService,
   NorthwindServiceServerSide>();
 
